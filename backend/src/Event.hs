7785a097feedcdff5556a6cee0860a4991dcd621
BlackCap <mailhph@gmail.com>
2018-12-10 19:42:37 +0100

    create events, profile page

--- backend/src/Event.hs
+++ backend/src/Event.hs
@@ -15,6 +15,14 @@ data Event = Event
   , date  :: UTCTime
   } deriving (Eq, Show, Generic, ToJSON, FromJSON, SqlRow)
 
+data MkEventReq = MkEventReq
+  { req_title :: Text
+  , req_desc  :: Text
+  , req_place :: Text
+  , req_date  :: UTCTime
+  } deriving (Eq, Show, Generic, ToJSON, FromJSON, SqlRow)
+
+
 data Participants = Participants
   { eid :: EventID
   , uid :: UserID
@@ -29,6 +37,7 @@ type Api = "events" :> Get '[JSON] [Event]
       :<|> "event"  :> QueryParam "id" EventID :> Get '[JSON] (Maybe Event)
       :<|> "participants" :> QueryParam "id" EventID :> Get '[JSON] [Participants]
       :<|> "pictures" :> QueryParam "id" EventID :> Get '[JSON] [Pictures]
+      :<|> "mkevent" :> ReqBody '[JSON] MkEventReq :> Post '[JSON] NoContent
 
 events :: Table Event
 events = table "events" [#eid :- autoPrimary]
@@ -50,10 +59,15 @@ server = do
     tryCreateTable pictures
 
   pure $ listEvents :<|> getEvent :<|> getParticipants :<|> getPictures
+                    :<|> mkEvent
 
   where listEvents      = db . query $ select events
         getEvent        = getByIDM db events       #eid
         getParticipants = getByID  db participants #eid
         getPictures     = getByID  db pictures     #eid
-
+        mkEvent     req = do
+          liftIO $ print req
+          db $ insert_ events
+            [Event def (toId 1) (req_title req) (req_desc req) (req_place req) (req_date req)]
+          return NoContent
 
