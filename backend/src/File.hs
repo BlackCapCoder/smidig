1c0fd04008758943d7d6cbaf2c09bfcaedd366bb
BlackCap <mailhph@gmail.com>
2018-12-12 16:17:09 +0100

    comment

--- backend/src/File.hs
+++ backend/src/File.hs
@@ -11,20 +11,26 @@ import Data.ByteString.Builder (byteString)
 type Api = Raw
 
 
+-- Serves files in ../frontend
+-- If html file, splice it into theme.html by replacing the % character
 server :: IO (Server Api)
 server = do
   (p1,p2) <- B.span (/=37) <$> B.readFile "../frontend/theme.html"
-  pure . (>>= pure . addTheme (byteString p1) (byteString $ B.tail p2))
-    $ serveDirectoryWebApp "../frontend"
 
-addTheme p1 p2 base req resp = base req (resp . trans)
-  where trans r
+  let p1' = byteString p1
+      p2' = byteString $ B.tail p2
+
+  pure . (>>= pure . addTheme p1' p2') $ serveDirectoryWebApp "../frontend"
+
+addTheme p1 p2 base req resp = base req $ resp . splice
+  where splice r
           | B.isSuffixOf ".html" $ rawPathInfo req
           , (status, headers, f) <- responseToStream r
           = responseStream status headers $ \write flush -> do
               write p1
               f $ \sb -> sb write flush
               write p2
+              flush
           | otherwise = r
 
 
